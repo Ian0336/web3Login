2d77073451041c6b704acdea88588b58ef40c8b5
YuYi Jhang <yuyi@YuYideMBP.home>
2024-09-19 14:12:05 +0800

    feat: frontend init

---
+++ web3Game/frontend/src/app/_components/Provider.tsx
@@ -0,0 +1,43 @@
+'use client';
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { ReactNode } from "react";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [sepolia],
+    transports: {
+      // RPC URL for each chain
+      [sepolia.id]: http(
+        `https://1rpc.io/sepolia`,
+      ),
+    },
+
+    // Required API Keys
+    walletConnectProjectId: "your-wallet-connect-project-id",
+
+    // Required App Info
+    appName: "Your App Name",
+
+    // Optional App Info
+    appDescription: "Your App Description",
+    appUrl: "localhost:3000", // your app's URL
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
+
+const queryClient = new QueryClient();
+
+
+export const Web3Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
